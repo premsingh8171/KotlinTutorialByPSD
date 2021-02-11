4bab9c5dbfbeeac60a50d20f038861dfb87e42f7
Er. prem singh daksha <46933759+premsingh8171@users.noreply.github.com>
2021-02-11 18:18:54 +0530

    DynamicAutoCompleteTextView

--- app/src/main/java/com/premsinghdaksha/kotlintutorials/MainActivity.kt
+++ app/src/main/java/com/premsinghdaksha/kotlintutorials/MainActivity.kt
@@ -1,30 +1,52 @@
 package com.premsinghdaksha.kotlintutorials
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val autotextView = findViewById<AutoCompleteTextView>(R.id.autoTextView)
-        // Get the array of languages
+        val autoTextView = AutoCompleteTextView(this)
+        val button = Button(this)
+        val layoutParams = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        autoTextView.layoutParams = layoutParams
+        button.layoutParams = layoutParams
+        layoutParams.setMargins(30, 30, 30, 30)
+        autoTextView.setHint(R.string.hint)
+        button.setText("Submit")
+
+        val linearLayout = findViewById<LinearLayout>(R.id.linear_layout)
+        //add AutoCompleteTextView and button to LinearLayout
+        linearLayout?.addView(autoTextView)
+        linearLayout?.addView(button)
+
+        //Get the array of languages
         val languages = resources.getStringArray(R.array.Languages)
         // Create adapter and add in AutoCompleteTextView
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, languages)
-       // autotextView.setAdapter(adapter)
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1, languages
+        )
+        autoTextView.setAdapter(adapter)
 
-        val button = findViewById<Button>(R.id.btn)
         if (button != null) {
-        button ?.setOnClickListener(View.OnClickListener {
-            val enteredText = getString(R.string.submitted_lang) + " " + autotextView.getText()
-        })
-    }
+            button?.setOnClickListener(View.OnClickListener {
+                val enteredText = getString(R.string.submitted_lang) + " " +
+                        autoTextView.getText()
+                Toast.makeText(
+                    this@MainActivity,
+                    enteredText, Toast.LENGTH_SHORT
+                ).show()
+            })
+        }
     }
 }

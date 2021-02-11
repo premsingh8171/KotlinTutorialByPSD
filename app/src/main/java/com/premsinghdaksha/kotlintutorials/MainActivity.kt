8577fb208b794aae339e4ee4c8af82842c4bf338
Er. prem singh daksha <46933759+premsingh8171@users.noreply.github.com>
2021-02-11 16:40:59 +0530

    Dynamic TextView

--- app/src/main/java/com/premsinghdaksha/kotlintutorials/MainActivity.kt
+++ app/src/main/java/com/premsinghdaksha/kotlintutorials/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.premsinghdaksha.kotlintutorials
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -9,10 +14,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView = findViewById(R.id.text_view_id) as TextView
 
-        textView?.setOnClickListener {
-            Toast.makeText(this@MainActivity, "COMPUTER SCIENCE PORTAL", Toast.LENGTH_LONG).show()
+        val layout = findViewById<RelativeLayout>(R.id.root);
+        val textView = TextView(this)
+
+        textView.layoutParams = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+
+        textView.setText("Hello Prem Singh")
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 40f)
+        textView.setTextColor(Color.MAGENTA)
+
+        textView.setOnClickListener() {
+            Toast.makeText(this@MainActivity, "Hello ", Toast.LENGTH_LONG).show()
+
         }
+        layout?.addView(textView)
+
     }
 }

48e6e3f4307d6da53dbb94b825203a77cc79b76a
Er. prem singh daksha <46933759+premsingh8171@users.noreply.github.com>
2021-02-14 14:46:46 +0530

    val num1 = findViewById<EditText>(R.id.Num1)

--- app/src/main/java/com/premsinghdaksha/kotlintutorials/MainActivity.kt
+++ app/src/main/java/com/premsinghdaksha/kotlintutorials/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.premsinghdaksha.kotlintutorials
 
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,20 +11,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imageButton = findViewById<ImageButton>(R.id.imageBtn)
         val num1 = findViewById<EditText>(R.id.Num1)
-        val num12 = findViewById<EditText>(R.id.Num2)
+        val num2 = findViewById<EditText>(R.id.Num2)
+        val imgBtn = ImageButton(this)
 
-        imageButton?.setOnClickListener()
-        {
-            if (num1.text.toString().isEmpty() || num12.text.toString().isEmpty()) {
-                Toast.makeText(this@MainActivity, R.string.pleaseenter, Toast.LENGTH_LONG).show()
-            } else {
+        imgBtn.layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 150)
+        imgBtn.setImageResource(R.drawable.ic_launcher_background)
+
+        val linearLayout = findViewById<LinearLayout>(R.id.linear_layout)
+        // Adding ImageButton in LinearLayout
+        linearLayout.addView(imgBtn)
+
+        imgBtn.setOnClickListener {
+            if (num1.text.toString().isEmpty() || num2.text.toString().isEmpty()) {
+                Toast.makeText(applicationContext, "Enter both numbers", Toast.LENGTH_SHORT).show()
+            }
+            else {
                 val num1 = Integer.parseInt(num1.text.toString())
-                val num2 = Integer.parseInt(num12.text.toString())
-                Toast.makeText(applicationContext,
-                        "Sum of the numbers = " + (num1 + num2),
-                        Toast.LENGTH_SHORT).show()
+                val num2 = Integer.parseInt(num2.text.toString())
+                Toast.makeText(applicationContext, "Multiplication of the numbers = " + (num1 * num2), Toast.LENGTH_SHORT).show()
             }
         }
     }

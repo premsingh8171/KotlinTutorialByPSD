124770e313ae3c5b96be5766d017a9c338dd8837
Er. prem singh daksha <46933759+premsingh8171@users.noreply.github.com>
2021-02-14 14:12:52 +0530

    push Button code

--- app/src/main/java/com/premsinghdaksha/kotlintutorials/MainActivity.kt
+++ app/src/main/java/com/premsinghdaksha/kotlintutorials/MainActivity.kt
@@ -10,31 +10,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val checkTextView = findViewById<CheckedTextView>(R.id.ctv)
-        if (checkTextView != null) {
-            checkTextView.isChecked = false
-            checkTextView.setCheckMarkDrawable(R.drawable.checked)
-
-            checkTextView.setOnClickListener {
-                checkTextView.isChecked = !checkTextView.isChecked
-                checkTextView.setCheckMarkDrawable(
-                    if (checkTextView.isChecked)
-                        android.R.drawable.checkbox_on_background
-                    else
-                        android.R.drawable.checkbox_off_background
-                )
-
-                val msg = getString(R.string.msg_shown) + " " +
-                        getString(
-                            if (checkTextView.isChecked)
-                                R.string.checked else R.string.unchecked
-                        )
-                Toast.makeText(
-                    this@MainActivity, msg,
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
+        val button = findViewById<Button>(R.id.button)
+        button?.setOnClickListener()
+        {
+            Toast.makeText(this@MainActivity, R.string.message, Toast.LENGTH_LONG).show()
         }
-
     }
 }

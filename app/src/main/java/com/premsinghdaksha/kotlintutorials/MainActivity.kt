dcae95e28ca21be781ad8fdf2de8775cf9c0015c
Er. prem singh daksha <46933759+premsingh8171@users.noreply.github.com>
2021-02-14 14:30:37 +0530

    push code of imageButton

--- app/src/main/java/com/premsinghdaksha/kotlintutorials/MainActivity.kt
+++ app/src/main/java/com/premsinghdaksha/kotlintutorials/MainActivity.kt
@@ -10,10 +10,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button)
-        button?.setOnClickListener()
+        val imageButton = findViewById<ImageButton>(R.id.imageBtn)
+        val num1 = findViewById<EditText>(R.id.Num1)
+        val num12 = findViewById<EditText>(R.id.Num2)
+
+        imageButton?.setOnClickListener()
         {
-            Toast.makeText(this@MainActivity, R.string.message, Toast.LENGTH_LONG).show()
+            if (num1.text.toString().isEmpty() || num12.text.toString().isEmpty()) {
+                Toast.makeText(this@MainActivity, R.string.pleaseenter, Toast.LENGTH_LONG).show()
+            } else {
+                val num1 = Integer.parseInt(num1.text.toString())
+                val num2 = Integer.parseInt(num12.text.toString())
+                Toast.makeText(applicationContext,
+                        "Sum of the numbers = " + (num1 + num2),
+                        Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
